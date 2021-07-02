99f7a0c23f5295094c25b6b6fdea4d1326a32ba4
Dhwanil Ditani <44235738+dhwanil-ditani@users.noreply.github.com>
2021-07-02 22:00:26 +0000

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+// import logo from './logo.svg';
+import { Navbar, NavbarBrand } from "reactstrap";
 
-class App extends Component {
-  constructor(props) {
+import Menu from './components/MenuComponent';
+
+import './App.css';
+import { DISHES } from './shared/dishes'
+
+class App extends Component{
+
+  constructor(props){
     super(props);
+
     this.state = {
       dishes: DISHES
     };
-  }
-  render() {
-    return (
-      <div className="App">
+
+  } 
+
+  render(){
+
+    return(
+      <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+              <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+
+        <Menu dishes={ this.state.dishes } />
       </div>
     );
+
   }
+
 }
 
 export default App;
